aa305c0a1e57834553670f498e303afeba141e7d
Fábio Varela <fabio-varela@protonmail.com>
2024-08-27 21:46:39 -0300

    Fix default url for development, add profile image to user, configure permitted parameters and after sign up redirect, changed some texts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+  has_one_attached :profile_image
+
   devise :database_authenticatable, :registerable, :confirmable, :validatable,
          :recoverable, :rememberable, :timeoutable, :lockable, :trackable, :omniauthable
 
